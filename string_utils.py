a547f06c0111ce5ceee9b76cfb829c295573ef6b
shelliaronov <shelliaronov24@gmail.com>
2025-11-14 09:19:41 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,30 +1,35 @@
 def split_before_each_uppercases(formula):
     start = 0
     split_formula = []
-    return split_formula
-
-    for end in range(1, len(formula)): 
-      if formula[end].isupper():
-        split_formula.append(formula[start:end])
-        start = end
-
-      
+    
+    for end in range(1, len(formula)):
+        if formula[end].isupper():
+            split_formula.append(formula[start:end])
+            start = end
+            
     split_formula.append(formula[start:])
     return split_formula
 
 
-def test_split_at_first_digit(formula):
-  digit_location = 1
 
-  while digit_location < len(formula) :
-    if formula[digit_location].isdigit():
-      break
-    digit_location += 1
 
-  if digit_location == len(formula) :
-    return formula, 1
-  else:
-    prefix = formula[:digit_location]
-    numeric_part_str = formula[digit_location:]
-    numeric_part_int = int(numeric_part_str)
-    return prefix, numeric_part_int
+def split_at_first_digit(formula):
+    digit_location = 0
+
+    while digit_location < len(formula):
+        if formula[digit_location].isdigit():
+            break
+        digit_location += 1
+
+    if digit_location == len(formula):
+        return formula, 1
+    else:
+        prefix = formula[:digit_location]
+        numeric_part_str = formula[digit_location:]
+        
+        try:
+            numeric_part_int = int(numeric_part_str)
+        except ValueError:
+            return prefix, 1
+            
+        return prefix, numeric_part_int
